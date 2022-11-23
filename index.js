0c108c5edcff9523278353d342eb558606e3ca51
anshulnegii <nanshul2003@gmail.com>
2022-11-24 00:46:48 +0530

    changed path for public

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const app = express();
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
+app.use(express.static(__dirname + '/public'))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")      
