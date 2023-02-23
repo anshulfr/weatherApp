15af6be70708f86db069848c2d8f292e3b333d65
anshulnegii <nanshul2003@gmail.com>
2023-02-23 18:16:45 +0530

    handled invalid place error

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + req.body.place.replace(/ /g, "+") + "&appid=0806d34c8dfcd028a68592ab149fa9e4&units=metric"
-    try {
-        https.get(url, (response) => {
-            response.on("data", (data) => {
-                const weatherData = JSON.parse(data)
-                console.log(weatherData);
+    https.get(url, (response) => {
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data)
+            try {
                 const temp = weatherData.main.temp
                 const timezone = weatherData.timezone
                 const placeSearch = weatherData.name
@@ -36,12 +35,12 @@ app.post("/", (req, res) => {
                 res.render("result", {tempVar: temp, timezoneVar: timezone, placeSearchVar: placeSearch, 
                     iconVar: icon, feelsLikeVar: feelsLike, pressureVar: pressure, humidityVar: humidity, 
                     windSpeedVar: windSpeed, dayVar: day})
-                
-            })
+            } catch (error) {
+                res.send(`<h2>Invalid Place, Please try again</h2>`)
+            }
+            
         })
-    } catch (e) {
-        res.send(`<h2>Invalid Place, Please try again</h2>`)
-    }
+    })
 })
 
 app.listen(
