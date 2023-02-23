482237f5c38e21f026bacded3ecaaf973d5f0cc1
anshulnegii <nanshul2003@gmail.com>
2023-02-23 18:06:51 +0530

    minor bug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const https = require('https')
-const bodyParser = require('body-parser')
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public'))
 
